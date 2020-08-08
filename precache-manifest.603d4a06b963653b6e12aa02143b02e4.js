e1300d91ebe3479f7841d5827858a60492663adf
BenoitGaudieri <benalek@gmail.com>
2020-08-08 10:40:34 +0200

    Updates

---
+++ precache-manifest.603d4a06b963653b6e12aa02143b02e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5053b524d5e5905f8102",
+    "url": "/react-lights-out/static/css/main.e47e6534.chunk.css"
+  },
+  {
+    "revision": "5053b524d5e5905f8102",
+    "url": "/react-lights-out/static/js/main.5053b524.chunk.js"
+  },
+  {
+    "revision": "cd22fa7af0c3eaf7956e",
+    "url": "/react-lights-out/static/js/1.cd22fa7a.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "53b50314dd4b9db839334e33f79522f9",
+    "url": "/react-lights-out/index.html"
+  }
+];
