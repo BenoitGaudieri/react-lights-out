1ff413c3cf060381388f4976de351645aa22b874
BenoitGaudieri <benalek@gmail.com>
2020-08-08 10:39:33 +0200

    completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lights-out
+
+A little lights out game made with React.
+
+One of my first project using all base knowledge of React and lots of CSS styling.
---
+++ build/precache-manifest.6ea102b0890396e247ffe22580a9a7df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f744bc89919b19c7997",
+    "url": "/static/css/main.e47e6534.chunk.css"
+  },
+  {
+    "revision": "8f744bc89919b19c7997",
+    "url": "/static/js/main.8f744bc8.chunk.js"
+  },
+  {
+    "revision": "cd22fa7af0c3eaf7956e",
+    "url": "/static/js/1.cd22fa7a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6e0fc46ca2e69a652ac82540ec9271f",
+    "url": "/index.html"
+  }
+];
